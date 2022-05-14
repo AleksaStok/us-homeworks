5c60208aa123b17fda53f55e557f38c82efb761e
AleksaStok <75043007+AleksaStok@users.noreply.github.com>
2022-05-14 06:09:01 +0300

    Create index.js

---
+++ js-keyboard/index.js
@@ -0,0 +1,185 @@
+window.addEventListener("keydown", (event) => {
+  if (event.code === "KeyQ" && event.ctrlKey) {
+    let colorName;
+    colorName = document.getElementById("para");
+    colorName.innerHTML = "QUARTZ";
+    colorName.style.color = "#D9ADD4";
+    document.body.style.background = "#51414F";
+    event.preventDefault();
+    // } else if (event.code === "KeyW" && event.ctrlKey) {
+    // let colorName;
+    // colorName = document.getElementById("para");
+    // colorName.innerHTML = "WHITE";
+    // colorName.style.color = "#595959";
+    // document.body.style.background = "#FFFFFF";
+    // event.preventDefault();
+  } else if (event.code === "KeyE" && event.ctrlKey) {
+    let colorName;
+    colorName = document.getElementById("para");
+    colorName.innerHTML = "EMERALD";
+    colorName.style.color = "#245936";
+    document.body.style.background = "#50C878";
+    event.preventDefault();
+  } else if (event.code === "KeyR" && event.ctrlKey) {
+    let colorName;
+    colorName = document.getElementById("para");
+    colorName.innerHTML = "RED";
+    colorName.style.color = "#590000";
+    document.body.style.background = "#FF0000";
+    event.preventDefault();
+    // } else if (event.code === "KeyT" && event.ctrlKey) {
+    //   let colorName;
+    //   colorName = document.getElementById("para");
+    //   colorName.innerHTML = "TANGERINE";
+    //   colorName.style.color = "#593100";
+    //   document.body.style.background = "#F28500";
+    //   event.preventDefault();
+  } else if (event.code === "KeyY" && event.ctrlKey) {
+    let colorName;
+    colorName = document.getElementById("para");
+    colorName.innerHTML = "YELLOW";
+    colorName.style.color = "#595900";
+    document.body.style.background = "#FFFF00";
+    event.preventDefault();
+  } else if (event.code === "KeyU" && event.ctrlKey) {
+    let colorName;
+    colorName = document.getElementById("para");
+    colorName.innerHTML = "ULTRAMARINE";
+    colorName.style.color = "#021559";
+    document.body.style.background = "#0437F2";
+    event.preventDefault();
+  } else if (event.code === "KeyI" && event.ctrlKey) {
+    let colorName;
+    colorName = document.getElementById("para");
+    colorName.innerHTML = "INDIGO";
+    colorName.style.color = "#8600E6";
+    document.body.style.background = "#4B0082";
+    event.preventDefault();
+  } else if (event.code === "KeyO" && event.ctrlKey) {
+    let colorName;
+    colorName = document.getElementById("para");
+    colorName.innerHTML = "ORANGE";
+    colorName.style.color = "#593A00";
+    document.body.style.background = "#FFA500";
+    event.preventDefault();
+  } else if (event.code === "KeyP" && event.ctrlKey) {
+    let colorName;
+    colorName = document.getElementById("para");
+    colorName.innerHTML = "PINK";
+    colorName.style.color = "#59253F";
+    document.body.style.background = "#FF69B4";
+    event.preventDefault();
+  } else if (event.code === "KeyA" && event.shiftKey) {
+    let colorName;
+    colorName = document.getElementById("para");
+    colorName.innerHTML = "AMETHYST";
+    colorName.style.color = "#5A3C78";
+    document.body.style.background = "#9966CC";
+    event.preventDefault();
+  } else if (event.code === "KeyS" && event.shiftKey) {
+    let colorName;
+    colorName = document.getElementById("para");
+    colorName.innerHTML = "SALMON";
+    colorName.style.color = "#783E37";
+    document.body.style.background = "#FA8072";
+    event.preventDefault();
+  } else if (event.code === "KeyD" && event.shiftKey) {
+    let colorName;
+    colorName = document.getElementById("para");
+    colorName.innerHTML = "DANDELION";
+    colorName.style.color = "#787018";
+    document.body.style.background = "#F0E130";
+    event.preventDefault();
+  } else if (event.code === "KeyF" && event.shiftKey) {
+    let colorName;
+    colorName = document.getElementById("para");
+    colorName.innerHTML = "FUCHSIA";
+    colorName.style.color = "#780078";
+    document.body.style.background = "#FF00FF";
+    event.preventDefault();
+  } else if (event.code === "KeyG" && event.shiftKey) {
+    let colorName;
+    colorName = document.getElementById("para");
+    colorName.innerHTML = "GREEN";
+    colorName.style.color = "#007800";
+    document.body.style.background = "#00FF00";
+    event.preventDefault();
+  } else if (event.code === "KeyH" && event.shiftKey) {
+    let colorName;
+    colorName = document.getElementById("para");
+    colorName.innerHTML = "HAN PURPLE";
+    colorName.style.color = "#270C78";
+    document.body.style.background = "#5218FA";
+    event.preventDefault();
+  } else if (event.code === "KeyJ" && event.shiftKey) {
+    let colorName;
+    colorName = document.getElementById("para");
+    colorName.innerHTML = "JADE";
+    colorName.style.color = "#007850";
+    document.body.style.background = "#00A36C";
+    event.preventDefault();
+  } else if (event.code === "KeyK" && event.shiftKey) {
+    let colorName;
+    colorName = document.getElementById("para");
+    colorName.innerHTML = "KHAKI";
+    colorName.style.color = "#DED895";
+    document.body.style.background = "#8A865D";
+    event.preventDefault();
+  } else if (event.code === "KeyL" && event.shiftKey) {
+    let colorName;
+    colorName = document.getElementById("para");
+    colorName.innerHTML = "LAVENDER";
+    colorName.style.color = "#635178";
+    document.body.style.background = "#967BB6";
+    event.preventDefault();
+  } else if (event.code === "KeyZ" && event.altKey && event.ctrlKey) {
+    let colorName;
+    colorName = document.getElementById("para");
+    colorName.innerHTML = "ZUCCHINI";
+    colorName.style.color = "#28784E";
+    document.body.style.background = "#17462E";
+    event.preventDefault();
+  } else if (event.code === "KeyX" && event.altKey && event.ctrlKey) {
+    let colorName;
+    colorName = document.getElementById("para");
+    colorName.innerHTML = "XANTHIC";
+    colorName.style.color = "#787805";
+    document.body.style.background = "#EEED09";
+    event.preventDefault();
+  } else if (event.code === "KeyC" && event.altKey && event.ctrlKey) {
+    let colorName;
+    colorName = document.getElementById("para");
+    colorName.innerHTML = "CYAN";
+    colorName.style.color = "#007878";
+    document.body.style.background = "#00FFFF";
+    event.preventDefault();
+  } else if (event.code === "KeyV" && event.altKey && event.ctrlKey) {
+    let colorName;
+    colorName = document.getElementById("para");
+    colorName.innerHTML = "VIOLET";
+    colorName.style.color = "#440078";
+    document.body.style.background = "#8F00FF";
+    event.preventDefault();
+  } else if (event.code === "KeyB" && event.altKey && event.ctrlKey) {
+    let colorName;
+    colorName = document.getElementById("para");
+    colorName.innerHTML = "BLUE";
+    colorName.style.color = "#000078";
+    document.body.style.background = "#0000FF";
+    event.preventDefault();
+  } else if (event.code === "KeyN" && event.altKey && event.ctrlKey) {
+    let colorName;
+    colorName = document.getElementById("para");
+    colorName.innerHTML = "NAVAJO";
+    colorName.style.color = "#786951";
+    document.body.style.background = "#FFDEAD";
+    event.preventDefault();
+  } else if (event.code === "KeyM" && event.altKey && event.ctrlKey) {
+    let colorName;
+    colorName = document.getElementById("para");
+    colorName.innerHTML = "MAROON";
+    colorName.style.color = "#F70000";
+    document.body.style.background = "#800000";
+    event.preventDefault();
+  }
+});
